6b2956412fbe0e78dd2cb6438aaa112d53f5265e
Amanda <mandy.cavalcanti@gmail.com>
2021-01-19 20:26:08 -0300

    Adicionando estrutura do react

---
+++ pokedex/src/App.js
@@ -0,0 +1,20 @@
+import React from  'react';
+import Routes from './routes';
+
+
+
+
+
+function App() {
+
+
+
+  return (
+    <>
+    <Routes/>
+    </>
+
+  );
+}
+
+export default App;
---
+++ pokedex/src/Pages/Home/index.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+function App(props){
+
+
+
+
+return(
+
+<div>
+Ois galere
+</div>
+
+
+);
+}
+
+export default App;
