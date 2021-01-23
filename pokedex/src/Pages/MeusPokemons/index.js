d052b1289f7050515d2e48de374dfc591cece734
Amanda <mandy.cavalcanti@gmail.com>
2021-01-22 23:10:18 -0300

    Pegando pokemons a outra pagina

--- pokedex/src/Pages/MeusPokemons/index.js
+++ pokedex/src/Pages/MeusPokemons/index.js
@@ -1,12 +1,15 @@
-import React, { useEffect } from 'react';
-import Home from "../Pages/Home"
+import React, { useEffect, useState } from 'react';
+import Home from "../Home"
 
 export default function MeusPokemons(){
+  const [ pokemonsCapturados, setPokemonsCapturados] = useState([]);
+
   useEffect(()=>{
-    const pokemonsCapturados = localStorage.getItem('Pokemons Capturados');
+    let pokemons = localStorage.getItem('Pokemons Capturados');
+    console.log(pokemons);
 
   }, []);
-}
+
 
 return(
 
@@ -16,3 +19,4 @@ return(
 
 
 )
+}
